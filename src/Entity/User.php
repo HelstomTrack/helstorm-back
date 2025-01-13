c29daed68257333a4db9fd3aa471d5e1fb452290
Ilyes-DEO <ide.dno.lyon@gmail.com>
2025-01-13 21:45:42 +0100

    add jwt for secure api

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -50,7 +51,7 @@ class User implements PasswordAuthenticatedUserInterface
     /**
      * @var Collection<int, Program>
      */
-    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Program::class, cascade: ['persist', 'remove'])]
+    #[ORM\OneToMany(targetEntity: Program::class, mappedBy: 'user', cascade: ['persist', 'remove'])]
     #[Groups(['user'])]
     private Collection $programs;
 
@@ -223,5 +224,20 @@ class User implements PasswordAuthenticatedUserInterface
         $this->phoneNumber = $phoneNumber;
 
         return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return ["ROLE_USER"];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 }
